7b287904168bcb1baf2f3870e2921831fa654f01
Shihabuddin9 <shihab.bd.pk@gmail.com>
2022-05-10 11:10:52 +0600

    Banner setup in home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './pages/Shared/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/HomePage/Home/Home';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
