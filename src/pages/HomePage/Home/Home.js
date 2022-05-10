e73ae11fce65f3d95f52ee762ad45ebec583b57e
Shihabuddin9 <shihab.bd.pk@gmail.com>
2022-05-10 17:30:35 +0600

    contact setup

--- src/pages/HomePage/Home/Home.js
+++ src/pages/HomePage/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
             <Services></Services>
             <MakeAppointment></MakeAppointment>
             <Testimonial></Testimonial>
-            {/* <Contact></Contact> */}
+            <Contact></Contact>
         </div>
     );
 };
